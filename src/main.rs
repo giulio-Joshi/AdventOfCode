bce0fe37c6d73d6c9bcecd143d3e61c5c3f75211
giulio-Joshi <me@joshi.it>
2022-12-01 09:18:53 +0100

    ✅ Day 1

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,20 @@ fn main() {
         .expect("Unable to parse file");
 
     let stacks = split_stacks(&raw_input);
-    let elven_amount = calc_max_sum(stacks);
-    println!("Found the max amount of snacks: {}", elven_amount.iter().max().unwrap());
+    let mut elven_amount = calc_max_sum(stacks);
+    println!("Found the max amount of snacks: {}", get_top_snack_holder(&mut elven_amount,1));
+    println!("Found the three max amount of snacks: {}", get_top_snack_holder(&mut elven_amount,3));
+}
+
+fn get_top_snack_holder( elven_list: &mut [u32] , number: usize) -> u32 {
+
+    elven_list.sort_by(|a,b | b.cmp(a));
+    let actual_max = if number > elven_list.len() {
+        elven_list.len()
+    } else { 
+        number
+    };
+    elven_list[0..actual_max].iter().sum()
 }
 
 fn calc_max_sum(elves: Vec<Vec<u32>>) -> Vec<u32> {
@@ -31,13 +43,13 @@ mod test {
     fn day1() {
         let input = data();
         let parsed = split_stacks(input);
-        //println!("{parsed:?}");
-        //assert_eq!(6, parsed.len());  // Should be 6 but I'm ignoring empty stacks for the first part
+        // Should be 6 but I'm ignoring empty stacks for the first part (proved to be unuseful)
+        //assert_eq!(6, parsed.len()); 
         assert_eq!(5, parsed.len());
 
-        let maxes = calc_max_sum(parsed);
-        println!("{maxes:?}");
-        assert_eq!(&24000, maxes.iter().max().unwrap());
+        let mut maxes = calc_max_sum(parsed);
+        assert_eq!(24000, get_top_snack_holder(&mut maxes,1 ));
+        assert_eq!(45000, get_top_snack_holder(&mut maxes,3 ));
     }
 
     fn data() -> &'static str {
