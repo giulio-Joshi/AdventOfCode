111c6969217fe96256ac77ba9761995114f86395
giulio-Joshi <me@joshi.it>
2022-12-03 11:16:22 +0100

    ✅ Day3 gone.
    Some things I should handle better, like string handling

--- src/day2.rs
+++ src/day2.rs
@@ -3,17 +3,77 @@ use core::panic;
 pub fn execute(raw_input: &str) {
     let battles = read_battle(raw_input);
     let outcome = strategy_one( &battles );
+    let second = strategy_two( raw_input);
     println!("Resulting outcome strategy {outcome}");
+    println!("Resulting second strategy {second}");
 }
 
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone,Debug)]
+enum Outcome {
+    Win,
+    Draw,
+    Lose
+}
+
+impl Outcome {
+    fn reverse(&self, other: &Sign ) ->u32 {
+
+        match self {
+            Self::Win => {
+                6+ match other {
+                    Sign::Rock => 2,
+                    Sign::Paper => 3,
+                    Sign::Scissor => 1,
+                }
+            },
+            Self::Draw => {
+                3 + match other {
+                    Sign::Rock => 1,
+                    Sign::Paper => 2,
+                    Sign::Scissor => 3,
+                }
+            },
+            Self::Lose => {
+                match other {
+                    Sign::Rock => 3,
+                    Sign::Paper => 1,
+                    Sign::Scissor => 2,
+                }
+            }
+        }
+
+    }
+}
+
+
+#[derive(Copy,Clone,Debug)]
 enum Sign {
     Rock,
     Paper,
     Scissor,
 }
 
+impl From<&str> for Outcome {
+    fn from(input: &str) -> Self {
+        match input  {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => panic!("unexpected value")
+        }
+    }
+}
+
 impl Sign {
+
+    fn value( &self ) -> u32 {
+        match self {
+            Sign::Rock => 1,
+            Sign::Paper => 2,
+            Sign::Scissor => 3
+        }
+    }
+
     fn score(&self, other: &Sign) -> u32 {
         match self {
             Self::Rock => {
@@ -39,11 +99,12 @@ impl Sign {
             }
         }
     }
+
 }
 
-impl From<String> for Sign {
-    fn from(input: String) -> Self {
-        match input.as_str()  {
+impl From<&str> for Sign {
+    fn from(input: &str) -> Self {
+        match input  {
             "A" => Sign::Rock,
             "B" => Sign::Paper,
             "C" => Sign::Scissor,
@@ -55,13 +116,36 @@ impl From<String> for Sign {
     }
 }
 
+fn strategy_two(input: &str) -> u32 {
+
+    let battles : Vec<(Sign,Outcome)> = input.split('\n')
+        .map(|v| {
+            let stuff : Vec<&str> = v.split(' ').collect();
+            (Sign::from(stuff[0]), Outcome::from(stuff[1]))
+        }
+        )
+        .collect();
+
+    guess_score( &battles)
+}
+
+fn guess_score(battles : &[(Sign, Outcome)] ) -> u32 {
+
+    battles.iter()
+        .map(|x| {
+            x.1.reverse(&x.0)
+        } )
+        .sum()
+
+}
+
 
 fn read_battle(input: &str) -> Vec<(Sign, Sign)> {
 
     input.split('\n')
         .map(|v| {
             let stuff : Vec<Sign> = v.split(' ')
-            .map( |s| Sign::from(s.to_owned()))
+            .map(Sign::from)
             .collect();
             (stuff[0], stuff[1])
         }
@@ -78,11 +162,7 @@ fn strategy_one( battles: &[(Sign,Sign)]) -> u32 {
 
 fn single_match_score( battle: &(Sign,Sign) ) -> u32 {
 
-    battle.1.score(&battle.0) + match &battle.1 {
-        Sign::Rock => 1,
-        Sign::Paper => 2,
-        Sign::Scissor => 3
-    }
+    battle.1.score(&battle.0) + battle.1.value()
 }
 
 #[cfg(test)]
@@ -97,6 +177,7 @@ mod test {
         assert_eq!(3 , battles.len());
 
         assert_eq!(15, strategy_one( &battles ));
+        assert_eq!(12, strategy_two( &data));
     }
 
     fn data() -> &'static str {
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::fs;
 
 mod day1;
 mod day2;
-
+mod day3;
 
 fn main() {
     //let raw_input = fs::read_to_string("data/day1.txt").expect("Unable to parse file");
     //day1::execute(&raw_input);
 
-    let raw_input = fs::read_to_string("data/day2.txt").expect("Unable to parse file");
-    day2::execute(&raw_input);
+    //let raw_input = fs::read_to_string("data/day2.txt").expect("Unable to parse file");
+    //day2::execute(&raw_input);
+
+    let raw_input = fs::read_to_string("data/day3.txt").expect("Unable to parse file");
+    day3::execute(&raw_input);
 }
